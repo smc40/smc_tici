5ca35f65d4a6689e0c8f957626014296e8e0df7f
nicoapg <40054301+nicoapg@users.noreply.github.com>
2021-08-25 13:40:43 +0200

    Update search.py

--- search.py
+++ search.py
@@ -81,10 +81,7 @@ def collapse_sources(df_drugs_identified):
 
 
 def read_medicament_file(chosen_sources):
-    print(f'The current directory is {os.listdir()}')
-    print(f'Here...')
-    print(pathlib.Path().resolve())
-    print(f'The current directory static is {os.listdir("/static/")}')
+    print(f'The current directory static is {os.listdir(_MYPATH + "/static/")}')
     """
     To read file from any of the four sources ['fda','rxnorm','usan','swissmedic']
 
