8f5947774ddd8d16f9c3884c4fbbd705ef2646de
nicoapg <40054301+nicoapg@users.noreply.github.com>
2021-08-25 09:57:27 +0200

    Update search.py

--- search.py
+++ search.py
@@ -75,7 +75,7 @@ def collapse_sources(df_drugs_identified):
 
 
 def read_medicament_file(chosen_sources):
-    print(f'The current directory is {os.listdir}')
+    print(f'The current directory is {os.listdir()}')
     """
     To read file from any of the four sources ['fda','rxnorm','usan','swissmedic']
 
